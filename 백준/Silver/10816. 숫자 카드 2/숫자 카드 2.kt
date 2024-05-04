2cdf8f6aa3839000f6ed61f78c2d0037412e41c4
honggi <89631493+honggi123@users.noreply.github.com>
2024-05-04 19:31:35 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 2208 ms, Memory: 316304 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.kt
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.kt
@@ -1,32 +1,86 @@
 import java.util.*;
-import java.io.*;
 
 // Map 사용
-fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
-    val bw = BufferedWriter(OutputStreamWriter(System.out))
 
-    val n = readLine().toInt()
-
-    val inputN = readLine().split(" ").map {it.toInt()}
-
-    val m = readLine().toInt()
-
-    val inputM = readLine().split(" ").map {it.toInt()}
-
-    val hashMap = HashMap<Int,Int>()
-
-    repeat(n){
-       hashMap[inputN[it]] = hashMap.getOrDefault(inputN[it],0) + 1;    
-    }
-
-    repeat(m) {
-        bw.write("${hashMap[inputM[it]] ?: 0} ")
-    }
-
-    bw.flush()
-    bw.close()
-}
+//    fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+//        val bw = BufferedWriter(OutputStreamWriter(System.out))
+//
+//        val n = readLine().toInt()
+//
+//        val inputN = readLine().split(" ").map {it.toInt()}
+//
+//        val m = readLine().toInt()
+//
+//        val inputM = readLine().split(" ").map {it.toInt()}
+//
+//        val hashMap = HashMap<Int,Int>()
+//
+//        repeat(n){
+//            hashMap[inputN[it]] = hashMap.getOrDefault(inputN[it],0) + 1;
+//        }
+//
+//        repeat(m) {
+//            bw.write("${hashMap[inputM[it]] ?: 0} ")
+//        }
+//
+//        bw.flush()
+//        bw.close()
+//    }
 
 // 이진탐색 
+fun main() = with(Scanner(System.`in`)){
 
+    val strBuilder = StringBuilder()
+    val N = nextInt()
+    val array = IntArray(N)
+
+    for (i in 0 until N){
+        array[i] = nextInt()
+    }
+
+    Arrays.sort(array)
+
+    val M = nextInt()
+
+    for (i in 0 until M){
+        val key = nextInt()
+
+        strBuilder.append(upperBound(array, key) - lowerBound(array, key)).append(' ')
+    }
+    println(strBuilder)
+}
+
+fun lowerBound(array: IntArray, key: Int): Int {
+    var left = 0
+    var right = array.size
+
+    while (left < right){
+        val midIndex = (left + right) / 2
+        val target = array[midIndex]
+
+        if (key <= target){
+            right = midIndex
+        } else{
+            left = midIndex + 1
+        }
+    }
+    return left
+}
+
+fun upperBound(array: IntArray, key: Int): Int {
+    var left = 0
+    var right = array.size
+
+    while (left < right){
+        val midIndex = (left + right) / 2
+        val target = array[midIndex]
+
+        if (key < target){
+            right = midIndex
+        } else{
+            left = midIndex + 1
+        }
+    }
+    return left
+}
 
