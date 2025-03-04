b6fac9daadd83bf9b7eefbf23810b1c76e88e693
honggi <89631493+honggi123@users.noreply.github.com>
2025-03-04 17:50:44 +0900

    [Silver III] Title: 주유소, Time: 356 ms, Memory: 40380 KB, Score: 17 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.kt
@@ -0,0 +1,22 @@
+fun main(){
+   val br = System.`in`.bufferedReader()
+   val bw = System.out.bufferedWriter()
+      
+   var sum = 0L
+   val num = br.readLine().toInt()
+   var lengthArray = br.readLine().split(" ").map{ it.toLong() }
+   val priceArray = br.readLine().split(" ").map{ it.toLong() }
+   var minPrice = priceArray[0]
+       
+   priceArray.forEachIndexed { index, price ->
+       if(minPrice < price) {
+           minPrice = price
+       }
+       if(index != priceArray.size - 1){
+          sum += minPrice * lengthArray[index]   
+       }                 
+   }
+   bw.append("${sum}")
+   bw.flush()
+   bw.close()
+}
