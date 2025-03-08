0a1505401ed677b198834a832aaf67510fd0c914
honggi <89631493+honggi123@users.noreply.github.com>
2025-03-08 14:24:51 +0900

    [Silver V] Title: 단어 정렬, Time: 480 ms, Memory: 45556 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.kt
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.kt
@@ -4,19 +4,16 @@ fun main(){
         
     val n = br.readLine().toInt()
     val stringSet = mutableSetOf<String>() 
-    val wordSet = mutableSetOf<Pair<Int,String>>()  
         
     repeat(n){
        val s = br.readLine().toString()
        stringSet.add(s) 
     }    
-    stringSet.forEach {
-        wordSet.add(Pair(it.length,it))
-    }
-    val sortedSet = wordSet.sortedWith(compareBy({ it.first },{ it.second }))
+
+    val sortedSet = stringSet.sortedWith(compareBy({ it.length },{ it }))
     
     sortedSet.forEach{
-       bw.append("${it.second}\n")
+       bw.append("${it}\n")
     }    
     bw.flush()
     bw.close()
